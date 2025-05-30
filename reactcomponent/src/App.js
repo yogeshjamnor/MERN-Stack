6baf688d0dbf5088b913377e745b8167f51c25ac
Yogesh <yogeshjamnorl@gmail.com>
2025-05-30 14:58:52 +0530

    Add initial MERN stack project

---
+++ reactcomponent/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+function App() {
+  return (
+    <>
+      <h1>First demo</h1>
+    </>
+  );
+}
+
+export default App;
