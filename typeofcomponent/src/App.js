7fe2f549025e78eaa1b32a12249a7abe85739883
Yogesh <yogeshjamnorl@gmail.com>
2025-06-02 15:13:34 +0530

    update from vs code

--- typeofcomponent/src/App.js
+++ typeofcomponent/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function App() {
-  return (
-    <>
-      <h1>hello yogesh</h1>
-    </>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { id: 1 };
+  }
+  render() {
+    return (
+      <>
+        <h1>hello yogesh {this.state.id}</h1>
+      </>
+    );
+  }
 }
