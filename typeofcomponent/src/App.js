04c69e9b1f0fbe7400571d0c34710563f980a61e
Yogesh <yogeshjamnorl@gmail.com>
2025-06-03 15:25:24 +0530

    Your commit message

--- typeofcomponent/src/App.js
+++ typeofcomponent/src/App.js
@@ -6,11 +6,15 @@ export default class App extends React.Component {
     this.state = { id: 1, name: 'ABC' };
   }
   render() {
+    const Demo1 = () => {
+      this.setState({ name: 'yogesh' });
+    };
     return (
       <>
         <h1>
           hello {this.state.name}! your id is: {this.state.id}
         </h1>
+        <button onClick={() => Demo1()}>change state</button>
       </>
     );
   }
