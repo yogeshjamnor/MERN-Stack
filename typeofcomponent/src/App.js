d0f45086226c0108a333221bc21e8205f2665da3
Yogesh <yogeshjamnorl@gmail.com>
2025-06-03 16:05:47 +0530

    Your commit message

--- typeofcomponent/src/App.js
+++ typeofcomponent/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = { id: 1, name: 'ABC' };
+    this.state = { id: 1, name: 'ABC', count: 0 };
   }
   render() {
     const Demo1 = () => {
@@ -15,6 +15,14 @@ export default class App extends React.Component {
           hello {this.state.name}! your id is: {this.state.id}
         </h1>
         <button onClick={() => Demo1()}>change state</button>
+        <hr />
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Add 1
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+          reduce 1
+        </button>
       </>
     );
   }
